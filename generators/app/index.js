681d4920773542110421a600d33da408f41f99a5
empun <maftukhatularifin13@gmail.com>
2022-04-16 06:56:38 +0700

    feat: add submodule generator files

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,42 +1,61 @@
 'use strict';
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
-const yosay = require('yosay');
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(
-        `Welcome to the groundbreaking ${chalk.red(
-          'generator-gulp-starter-project'
-        )} generator!`
-      )
-    );
-
-    const prompts = [
-      {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true,
-      },
-    ];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
+  welcome() {
+    this.log(`Welcome to ${chalk.green('gulp-starter-project')} generator!`);
+    this.log(`${chalk.green.bold('Generating files ... 🍜')}`);
   }
 
   writing() {
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath('_gitignore'),
+      this.destinationPath('.gitignore')
     );
+    this.fs.copy(
+      this.templatePath('.eslintrc.js'),
+      this.destinationPath('.eslintrc.js')
+    );
+    this.fs.copy(
+      this.templatePath('.gitattributes'),
+      this.destinationPath('.gitattributes')
+    );
+    this.fs.copy(
+      this.templatePath('gulpfile.js'),
+      this.destinationPath('gulpfile.js')
+    );
+    this.fs.copy(
+      this.templatePath('LICENSE.txt'),
+      this.destinationPath('LICENSE.txt')
+    );
+    this.fs.copy(
+      this.templatePath('package-lock.json'),
+      this.destinationPath('package-lock.json')
+    );
+    this.fs.copy(
+      this.templatePath('package.json'),
+      this.destinationPath('package.json')
+    );
+    this.fs.copy(
+      this.templatePath('README.md'),
+      this.destinationPath('README.md')
+    );
+    this.fs.copy(this.templatePath('src'), this.destinationPath('src'));
   }
 
-  install() {
-    this.installDependencies();
+  Install() {
+    this.npmInstall();
+  }
+
+  end() {
+    this.log(' ');
+    this.log(`${chalk.blue.bold('Generating project has finished! 🚀')}`);
+    this.log('Project was generated from 🔻 ');
+    this.log('https://github.com/empun/gulp-starter-project');
+    this.log(
+      `run ${chalk.green.bold('npm run dev')} to start development server`
+    );
+    this.log(' ');
   }
 };
