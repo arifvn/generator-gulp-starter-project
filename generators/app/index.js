5579711ce586a4ca151f4e42a8a08151a910cba7
empun <maftukhatularifin13@gmail.com>
2022-04-16 07:34:31 +0700

    refactor: update generator files

--- generators/app/index.js
+++ generators/app/index.js
@@ -41,6 +41,10 @@ module.exports = class extends Generator {
       this.templatePath('README.md'),
       this.destinationPath('README.md')
     );
+    this.fs.write(
+      this.destinationPath('README.md'),
+      `This project was generated from [https://github.com/empun/gulp-starter-project.git](https://github.com/empun/gulp-starter-project.git)`
+    );
     this.fs.copy(this.templatePath('src'), this.destinationPath('src'));
   }
 
